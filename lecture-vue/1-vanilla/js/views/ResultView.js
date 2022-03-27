cc66c4d397f3eed2bc842ebf52910da709473a99
kimsongjun <kinsongjun13@gmail.com>
2022-03-27 17:23:10 +0900

    [ADD] 0327 검색폼 구현

--- lecture-vue/1-vanilla/js/views/ResultView.js
+++ lecture-vue/1-vanilla/js/views/ResultView.js
@@ -8,28 +8,23 @@ ResultView.messages = {
   NO_RESULT: '검색 결과가 없습니다'
 }
 
+// view 객체의 init 메소드
 ResultView.setup = function (el) {
   this.init(el)
 }
 
+// 서버에서 검색결과 데이터를 받아서 동적으로 
+// 데이터는 컬렉션으로 가져옴
 ResultView.render = function (data = []) {
   console.log(tag, 'render()', data)
+  // 데이터가 있을경우 / 없을경우
+  // 배열의 길이가 있을경우
   this.el.innerHTML = data.length ? this.getSearchResultsHtml(data) : this.messages.NO_RESULT
-  this.show()
+  // this.show()
 }
 
 ResultView.getSearchResultsHtml = function (data) {
-  return data.reduce((html, item) => {
-    html += this.getSearchItemHtml(item)
-    return html
-  }, '<ul>') + '</ul>'
-}
-
-ResultView.getSearchItemHtml = function (item) {
-  return `<li>
-    <img src="${item.image}" />
-    <p>${item.name}</p>
-  </li>`
+  debugger
 }
 
 export default ResultView

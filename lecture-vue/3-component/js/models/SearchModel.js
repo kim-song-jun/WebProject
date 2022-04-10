111e2a40c5d8518439c09f3cbb4ce8117e14b32a
kimsongjun <kinsongjun13@gmail.com>
2022-04-10 18:10:32 +0900

    [ADD] 0410 FormComponent 구현

---
+++ lecture-vue/3-component/js/models/SearchModel.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    id: 1,
+    name: '[키친르쎌] 홈메이드 칠리소스 포크립 650g', 
+    image: 'https://cdn.pixabay.com/photo/2022/02/24/15/17/cat-7032641_960_720.jpg'
+  },
+  {
+    id: 2,
+    name: '[키친르쎌] 이탈리아 파티 세트 3~4인분',
+    image: 'https://cdn.pixabay.com/photo/2022/03/25/17/54/sakura-7091532_960_720.jpg'
+  }
+]
+
+export default {
+  list(query) {
+    return new Promise(res => {
+      setTimeout(()=> {
+        res(data)
+      }, 200);
+    })
+  }
+}

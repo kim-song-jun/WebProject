fb77daa1ab75fe2115fecca3c1e5c3b414b00500
kimsongjun <kinsongjun13@gmail.com>
2022-04-14 17:57:54 +0900

    [ADD] ResultComponent 구현

--- lecture-vue/3-component/js/components/FormComponent.js
+++ lecture-vue/3-component/js/components/FormComponent.js
@@ -8,13 +8,16 @@ export default {
   },
   methods: {
     onSubmit() {
-      this.$emit('@submit',this.inputValue.trim())
+      // trim == 공백 제거 함수
+      // emit은 다른 컴포넌트에게 이벤트를 전달하기 위해 사용할 수 있다.
+      this.$emit('@submit',this.inputValue.trim()) 
     },
     onKeyup() {
-
+      if(!this.inputValue.length) this.onReset()
     },
     onReset() {
-
+      this.inputValue = ''
+      this.$emit('reset')
     }
   }
 }
